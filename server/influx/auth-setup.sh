4b249bc9b0d925dfefda78a80ad9e0b357f7a9d7
blooburry <altaccstotzka031@gmail.com>
2024-06-23 16:58:33 +0200

    added a bash script to automatically set passwords in the config files

---
+++ server/influx/auth-setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Usage function
+usage() {
+  echo "Usage: $0 -e [prod|dev] -p <password> -t <token>"
+  exit 1
+}
+
+# Parse command line arguments
+while getopts "e:p:t:" opt; do
+  case ${opt} in
+    e)
+      ENVIRONMENT=$OPTARG
+      ;;
+    p)
+      PASSWORD=$OPTARG
+      ;;
+    t)
+      TOKEN=$OPTARG
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+# Validate the required arguments
+if [[ -z "$ENVIRONMENT" || -z "$PASSWORD" || -z "$TOKEN" ]]; then
+  usage
+fi
+
+# Set the telegraf configuration file based on the environment
+if [ "$ENVIRONMENT" == "dev" ]; then
+  TELEGRAF_CONF="./telegraf.dev.conf"
+elif [ "$ENVIRONMENT" == "prod" ]; then
+  TELEGRAF_CONF="./telegraf.prod.conf"
+else
+  echo "Invalid environment specified. Use 'prod' or 'dev'."
+  exit 1
+fi
+
+# Write the password to the Docker Compose file
+COMPOSE_FILE="compose.yaml"
+sed -i "s/DOCKER_INFLUXDB_INIT_PASSWORD: .*/DOCKER_INFLUXDB_INIT_PASSWORD: $PASSWORD/" $COMPOSE_FILE
+
+# Write the token to the telegraf configuration file
+sed -i "s/token = .*/token = \"$TOKEN\"/" $TELEGRAF_CONF
+
+# Modify the Docker Compose file to use the correct telegraf configuration file
+if [ "$ENVIRONMENT" == "dev" ]; then
+  sed -i "s|\./telegraf.prod.conf:/etc/telegraf/telegraf.conf|./telegraf.dev.conf:/etc/telegraf/telegraf.conf|" $COMPOSE_FILE
+elif [ "$ENVIRONMENT" == "prod" ]; then
+  sed -i "s|\./telegraf.dev.conf:/etc/telegraf/telegraf.conf|./telegraf.prod.conf:/etc/telegraf/telegraf.conf|" $COMPOSE_FILE
+fi
+
+echo "Configuration updated successfully."
