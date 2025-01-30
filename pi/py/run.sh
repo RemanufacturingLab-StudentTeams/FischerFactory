ea246f0c154226bd07fa3916fffe9a57582d1077
blooburry <altaccstotzka031@gmail.com>
2025-01-30 15:43:00 +0100

    [BUGFIX] Fixed typo in run script that caused Pipenv to behave inconsistently with environment variables

--- pi/py/run.sh
+++ pi/py/run.sh
@@ -28,21 +28,21 @@ fi
 # Add the remanufacturing lab root path in the .env files
 KEY="PROJECT_ROOT_PATH"
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-if grep -q "^$KEY=" ".env-prod"; then
+if grep -q "^$KEY=" ".env.prod"; then
     echo "$KEY found in $ENV_FILE. Updating its value."
-    sed -i "s|^$KEY=.*|$KEY=$PROJECT_ROOT|" ".env-prod"
+    sed -i "s|^$KEY=.*|$KEY=$PROJECT_ROOT|" ".env.prod"
 else
-    echo "$KEY not found in .env-prod. Adding it."
+    echo "$KEY not found in .env.prod. Adding it."
     # Append the key-value pair to the .env file
-    echo "$KEY=$PROJECT_ROOT" >> ".env-prod"
+    echo "$KEY=$PROJECT_ROOT" >> ".env.prod"
 fi
-if grep -q "^$KEY=" ".env-dev"; then
+if grep -q "^$KEY=" ".env.dev"; then
     echo "$KEY found in $ENV_FILE. Updating its value."
-    sed -i "s|^$KEY=.*|$KEY=$PROJECT_ROOT|" ".env-dev"
+    sed -i "s|^$KEY=.*|$KEY=$PROJECT_ROOT|" ".env.dev"
 else
-    echo "$KEY not found in .env-dev. Adding it."
+    echo "$KEY not found in .env.dev. Adding it."
     # Append the key-value pair to the .env file
-    echo "$KEY=$PROJECT_ROOT" >> ".env-dev"
+    echo "$KEY=$PROJECT_ROOT" >> ".env.dev"
 fi
 
 # Run the Python app in pipenv shell
